dda722086eccf31761dd78faddddd8b0729d9e61
Aaron Strick <strickinato@hyena.localdomain>
2024-05-08 17:00:44 -0700

    some basic noisemaking

--- src/Main.elm
+++ src/Main.elm
@@ -1,43 +1,198 @@
-module Main exposing (..)
+port module Main exposing (..)
 
 import Browser
-import Html exposing (Html)
+import Browser.Events
+import Css exposing (..)
+import Html.Styled as Html exposing (Attribute, Html)
+import Html.Styled.Attributes as Attributes
+import Html.Styled.Events as Events
+import Json.Decode as Decode
+import Keyboard.Event
+import Music.Pitch
+import Music.PitchClass
+import Music.Scale
+
+
+port sendNote : String -> Cmd msg
 
 
 main =
-    Browser.element
+    Browser.document
         { init = init
         , update = update
-        , view = view
+        , view = \model -> { body = [ view model |> Html.toUnstyled ], title = "hacker" }
         , subscriptions = subscriptions
         }
 
 
 type alias Model =
-    {}
+    { currentScale : Music.Scale.Scale
+    , currentPressedLetter : Maybe String
+    }
 
 
 type Msg
     = NoOp
+    | HandleKeyPress Keyboard.Event.KeyboardEvent
+    | ReleaseKey
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( {}, Cmd.none )
+    ( { currentScale = Music.Scale.major Music.PitchClass.c
+      , currentPressedLetter = Nothing
+      }
+    , Cmd.none
+    )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        HandleKeyPress keyboardEvent ->
+            let
+                degree =
+                    keyboardEvent.key
+                        |> Maybe.andThen frequencyMap
+            in
+            case degree of
+                Just degree_ ->
+                    let
+                        note =
+                            model.currentScale
+                                |> Music.Scale.degree degree_
+                                |> Music.Pitch.fromPitchClassInOctave 4
+                                |> Music.Pitch.toString
+                    in
+                    ( { model | currentPressedLetter = keyboardEvent.key }
+                    , sendNote note
+                    )
+
+                Nothing ->
+                    ( model, Cmd.none )
+
+        ReleaseKey ->
+            ( { model | currentPressedLetter = Nothing }, Cmd.none )
+
         NoOp ->
             ( model, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
-    Html.text "hi"
+    Html.div []
+        [ viewLetter |> maybeView model.currentPressedLetter
+        ]
+
+
+viewLetter : String -> Html Msg
+viewLetter str =
+    Html.div [] [ Html.text str ]
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Sub.batch
+        [ Browser.Events.onKeyDown (Decode.map HandleKeyPress Keyboard.Event.decodeKeyboardEvent)
+        , Browser.Events.onKeyUp (Decode.succeed ReleaseKey)
+        ]
+
+
+{-| letter to degree
+-}
+frequencyMap : String -> Maybe Int
+frequencyMap char =
+    case String.toUpper char of
+        " " ->
+            Just 1
+
+        "E" ->
+            Just 1
+
+        "A" ->
+            Just 1
+
+        "R" ->
+            Just 1
+
+        "I" ->
+            Just 3
+
+        "O" ->
+            Just 3
+
+        "T" ->
+            Just 3
+
+        "N" ->
+            Just 3
+
+        "S" ->
+            Just 5
+
+        "L" ->
+            Just 5
+
+        "C" ->
+            Just 5
+
+        "U" ->
+            Just 5
+
+        "D" ->
+            Just 5
+
+        "P" ->
+            Just 2
+
+        "M" ->
+            Just 2
+
+        "H" ->
+            Just 2
+
+        "G" ->
+            Just 4
+
+        "B" ->
+            Just 4
+
+        "F" ->
+            Just 4
+
+        "Y" ->
+            Just 4
+
+        "W" ->
+            Just 6
+
+        "K" ->
+            Just 6
+
+        "V" ->
+            Just 6
+
+        "X" ->
+            Just 6
+
+        "Z" ->
+            Just 7
+
+        "J" ->
+            Just 7
+
+        "Q" ->
+            Just 7
+
+        _ ->
+            Nothing
+
+
+maybeView : Maybe a -> (a -> Html msg) -> Html msg
+maybeView maybe view_ =
+    case maybe of
+        Just a ->
+            view_ a
+
+        Nothing ->
+            Html.text ""
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
+import * as Tone from "tone";
+
 const node = document.getElementById("mount");
 const app = Elm.Main.init({
   node,
 });
+
+const synth = new Tone.Synth().toDestination();
+
+app.ports.sendNote.subscribe((note) => {
+  synth.triggerAttackRelease(note, "8n");
+})
